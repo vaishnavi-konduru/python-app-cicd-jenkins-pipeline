2ab6c76139d056fe27c79387d818692d32726037
Vaishnavi Konduru <vaishnavi.konduru23@pcu.edu.in>
2026-02-01 21:23:14 +0530

    Remove xmlrunner and fix tests for pytest CI

--- test.py
+++ test.py
@@ -1,47 +1,32 @@
-import unittest
-import xmlrunner
 import app
 import re
+import unittest
 
 class TestHello(unittest.TestCase):
 
     def setUp(self):
         app.app.testing = True
-        self.app = app.app.test_client()
+        self.client = app.app.test_client()
 
-    def test_hello(self):
-
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-
-        # searching for <h1> tag in the application data using regex
+    def test_root(self):
+        rv = self.client.get('/')
+        assert rv.status_code == 200
 
         message = rv.data.decode()
-        start = re.search("<h1>",message).start()
-        end = re.search("</h1>",message).start()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
         message = message[start+4:end]
 
-        self.assertEqual(message, "hello brother, Lets have some fun!!")
+        assert message == "hello brother, Lets have some fun!!"
 
     def test_name(self):
-
         name = 'alice'
-        rv = self.app.get(f'/{name}')
-        self.assertEqual(rv.status, '200 OK')
-
-        # searching for <h1> tag in the application data using regex
+        rv = self.client.get(f'/{name}')
+        assert rv.status_code == 200
 
         message = rv.data.decode()
-        start = re.search("<h1>",message).start()
-        end = re.search("</h1>",message).start()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
         message = message[start+4:end]
 
-        self.assertEqual(message, "alice: A nerd")
-
-if __name__ == '__main__':
-
-    ############# To Generate Test Reports  #############
-    runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner)
-    #####################################################
-    unittest.main()
+        assert message == "alice: A nerd"
